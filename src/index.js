500c3daf567b55880d9e914bfd9adcedb31e482d
Gary Thung <garythung@gmail.com>
2019-02-07 17:11:45 -0500

    support unique name for import plugin

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 
-function rewireImport(config, env, importBabelOptions = {}) {
-  config = injectBabelPlugin(['import', importBabelOptions], config);
+function rewireImport(config, env, importBabelOptions = {}, name=undefined) {
+  if (name) {
+    config = injectBabelPlugin(['import', importBabelOptions, name], config);
+  } else {
+    config = injectBabelPlugin(['import', importBabelOptions], config);
+  }
+
   return config;
 }
 
